2ef5644a88e546b59e7a5d6566745f86f145121d
DIVYAJEET123 <divyajeet6393@gmail.com>
2024-11-15 19:50:03 +0530

    footer added

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -8,14 +8,12 @@ const StoreContextProvider = (props)=>{
 
     const [cartItems,setCartItems] = useState({});
 
-    const addToCart = (itemId)=>{
-        if(!cartItems[itemId]){
-            setCartItems((prev)=>({...prev,[itemId]:1}))
-        }
-        else{
-            setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
-        }
-    }
+    const addToCart = (itemId) => {
+        setCartItems((prev) => ({
+          ...prev,
+          [itemId]: (prev[itemId] || 0) + 1  // Update only the item specified by `itemId`
+        }));
+      };
     const removeFromCart = (itemId) => {
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
     }
