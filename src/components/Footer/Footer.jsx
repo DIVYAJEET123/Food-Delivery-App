4777957730bdee056cbbe58d5752a79d9477e597
DIVYAJEET123 <divyajeet6393@gmail.com>
2024-11-17 19:54:31 +0530

    cart and placeorder created successfully

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <div className='footer' id='footer'>
         <div className="footer-content">
             <div className="footer-content-left">
-                <img src={assets.logo} alt="" />
+                <img src={assets.logo_img2} alt="" />
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim quia ipsum, quam molestiae temporibus magni deserunt iusto, vitae nam doloribus quas ea illo molestias corrupti, sit voluptatem excepturi natus blanditiis?</p>
                 <div className="footer-social-icons">
                   <img src={assets.facebook_icon} alt="" />
